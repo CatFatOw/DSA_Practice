5ff5144e2bd1d9ca30510e56cf4fc58d6b261513
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2025-11-22 11:20:17 -0600

    Add files via upload

---
+++ sliding_window/1343_number_sub_array_avg.py
@@ -0,0 +1,18 @@
+# My solution
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+
+        curr = 0
+        ans = 0
+        for i in range(k):
+            curr += arr[i]
+        if curr / k >= threshold:
+            ans += 1
+        
+        for i in range(k, len(arr)):
+            curr += arr[i]
+            curr -= arr[i-k]
+            if curr / k >= threshold:
+                ans += 1
+        return ans
+        
