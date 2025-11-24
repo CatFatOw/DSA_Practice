bf80dd4b534f8e8b07b36acdc3021d5ced325d1e
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2025-11-24 00:29:51 -0600

    Add files via upload

---
+++ sliding_window/424_max_freq_replaced.py
@@ -0,0 +1,17 @@
+# My solution
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        mapping = defaultdict(int)
+        left = 0
+        max_len = 0
+        max_freq = 0
+
+        for right in range(len(s)):
+            mapping[s[right]] += 1
+            max_freq = max(max_freq, mapping[s[right]])
+            while right - left + 1 - max_freq > k:
+                mapping[s[left]] -=1
+                left += 1
+            max_len = max(max_len, right - left + 1)
+        return max_len
